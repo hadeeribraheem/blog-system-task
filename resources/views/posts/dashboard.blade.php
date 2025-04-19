e2930a5ebd468ba5b93506996bb188a258ea811f
Hadeer Ibraheem <hadeeribraheemothman@gmail.com>
2025-04-19 16:39:53 +0200

    edit Dashboard UI

--- resources/views/posts/dashboard.blade.php
+++ resources/views/posts/dashboard.blade.php
@@ -9,45 +9,50 @@
         <a href="{{ route('posts.create') }}" class="btn btn-success">+ New Post</a>
     </div>
 
-    <table id="dashboardTable" class="table table-bordered table-striped">
-        <thead class="table-dark">
-        <tr>
-            <th>#</th>
-            <th>Image</th>
-            <th>Title</th>
-            <th>Created</th>
-            <th>Updated</th>
-            <th>Actions</th>
-        </tr>
-        </thead>
-        <tbody>
-        @forelse ($posts_arr as $post)
+    @if(count($posts_arr))
+        <table id="dashboardTable" class="table table-bordered table-striped">
+            <thead class="table-dark">
             <tr>
-                <td>{{ $loop->iteration }}</td>
-                <td>
-                    {{-- Image Preview --}}
-                    <img src="{{ asset( 'images/' . ($post['image']['path'] ?? 'default.png')) }}"
-                         class="card-img-top img-fluid" alt="{{ $post['title'] }}"
-                         style="height: 50px; object-fit: cover;">
-
-                </td>
-                <td>{{ $post['title'] }}</td>
-                <td>{{ $post['created_at'] }}</td>
-                <td>{{ $post['updated_at'] ?? '-' }}</td>
-                <td>
-                    <a href="{{ route('posts.show', $post['id']) }}" class="btn btn-sm btn-info text-white"><i class="bi bi-eye"></i></a>
-                    <a href="{{ route('posts.edit', $post['id']) }}" class="btn btn-sm btn-primary"><i class="bi bi-pen"></i></a>
-                    <form action="{{ route('posts.destroy', $post['id']) }}" method="POST" class="d-inline" onsubmit="return confirm('Are you sure?')">
-                        @csrf
-                        @method('DELETE')
-                        <button class="btn btn-sm btn-danger"><i class="bi bi-trash"></i></button>
-                    </form>
-                </td>
+                <th>#</th>
+                <th>Image</th>
+                <th>Title</th>
+                <th>Created At</th>
+                <th>Last Update</th>
+                <th>Actions</th>
             </tr>
-        @empty
-            <tr><td colspan="5" class="text-center">No posts found.</td></tr>
-        @endforelse
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+            @foreach ($posts_arr as $post)
+                <tr>
+                    <td>{{ $loop->iteration }}</td>
+                    <td>
+                        <img src="{{ asset('images/' . ($post['image']['path'] ?? 'default.png')) }}"
+                             class="card-img-top img-fluid" alt="{{ $post['title'] }}"
+                             style="height: 50px; object-fit: cover;">
+                    </td>
+                    <td>{{ $post['title'] }}</td>
+                    <td>{{ $post['created_at'] }}</td>
+                    <td>{{ $post['updated_at'] ?? '-' }}</td>
+                    <td>
+                        <a href="{{ route('posts.show', $post['id']) }}" class="btn btn-sm btn-info text-white"><i class="bi bi-eye"></i></a>
+                        <a href="{{ route('posts.edit', $post['id']) }}" class="btn btn-sm btn-primary"><i class="bi bi-pen"></i></a>
+                        <form action="{{ route('posts.destroy', $post['id']) }}" method="POST" class="d-inline" onsubmit="return confirm('Are you sure?')">
+                            @csrf
+                            @method('DELETE')
+                            <button class="btn btn-sm btn-danger"><i class="bi bi-trash"></i></button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    @else
+        <div class="d-flex flex-column align-items-center justify-content-center">
+            <p class="text-center mt-4 text-muted">No posts to display.</p>
+            <img src="{{ asset('empty_dashboard.png') }}" class="w-50 img-fluid" alt="no posts found illustration">
+        </div>
+
+    @endif
+
 @endsection
 
